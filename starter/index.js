8fa1ececfc8bfbe6fb78dbf0c9b7f6230fc8ffa3
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-31 11:53:21 -0800

    added get requests to index.js

--- starter/index.js
+++ starter/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
-app.use(express.json());
+const { brotliDecompress } = require("zlib");
+
+const PORT = procces.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
 
 // GET Route for notes(notes.html) page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 // Wildcard route to direct users to a main(index.html) page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
+
+ // connects and sends a request to brotliDecompress.json
+app.post('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "db.json"));
+})

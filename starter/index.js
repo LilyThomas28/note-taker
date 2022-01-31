7f2afb39b7f261ab3513b8bf8ba65882d7026c1e
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-31 14:18:29 -0800

    Added last post in index.js to get api/notes

--- starter/index.js
+++ starter/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const path = require('path');
 const fs = require("fs");
-const { brotliDecompress } = require("zlib");
-
-const PORT = procces.env.PORT || 3001;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static('public'));
@@ -20,7 +17,34 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
- // connects and sends a request to brotliDecompress.json
+ // connects and sends a request to db.json
 app.post('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// creating post request that takes JSON input, "title" "text" and adds a new note object to the db.json file
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, response) => {
+        if (error) {
+            console.log(error);
+        }
+        const note = JSON.parse(response);
+        const noteReq = req.body;
+        const newNoteID = note.length + 1;
+        const newNote = {
+            id: newNoteID,
+            title: noteReq.title,
+            text: noteReq.text
+        };
+        notes.push(newNote);
+        res.json(newNote);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(note, null, 2), (err) => {
+            if (err) throw err;
+        });
+    });
+});
+
+// create listener to start the server
+app.listen(PORT, () => {
+    console.log(`App is listening on Port ${PORT}`);
 })
